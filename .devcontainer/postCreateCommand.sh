710ca97ac4b8e37a286a134b159abafbf7321bf9
Gordon Watts <gwatts@uw.edu>
2024-07-03 11:53:59 -0700

    Fix up the post-build action

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,4 +4,4 @@ mamba env create -f environment.yml
 
 echo "conda activate 2023-07-11-scipy-tutorial-advanced-numpy" >> /home/vscode/.bashrc
 
-cp /opt/conda/envs/2023-07-11-scipy-tutorial-advanced-numpy/bin/jupyter /opt/conda/bin/jupyter
+cp /opt/conda/envs/scipy2024-tutorial-thinking-in-arrays/bin/jupyter /opt/conda/bin/jupyter
